30eb242a332cd276a12448bf82d8d8d2c2a8c64f
Yared Terefe <yaredterefeg@gmail.com>
2022-08-04 01:28:42 +0300

    seed file has hidden username and password now

--- prisma/seed.js
+++ prisma/seed.js
@@ -1,17 +1,23 @@
 const { hash } = require("bcrypt");
 const { prisma } = require("../config");
+require("dotenv").config();
 
 const addUser = async () => {
+    if (!process.env.ADMIN_USERNAME || !process.env.ADMIN_PASSWORD) {
+        return console.log(
+            "please add admin username and password in the environment variables"
+        );
+    }
     await prisma.user.upsert({
         where: {
-            username: "tommydicktommy",
+            username: process.env.ADMIN_USERNAME,
         },
         update: {
-            username: "tommydicktommy",
+            username: process.env.ADMIN_USERNAME,
         },
         create: {
-            password: await hash("randomPassword", 10),
-            username: "tommydicktommy",
+            password: await hash(process.env.ADMIN_PASSWORD, 10),
+            username: process.env.ADMIN_USERNAME,
             isTommy: true,
         },
     });
